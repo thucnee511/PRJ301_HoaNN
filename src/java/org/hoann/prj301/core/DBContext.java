91b968eb0273616ddd406b9740da9c9b41b263e1
Neru <84850523+thucnee511@users.noreply.github.com>
2024-03-19 15:39:51 +0700

    refactor: DBContext.java file

--- src/java/org/hoann/prj301/core/DBContext.java
+++ src/java/org/hoann/prj301/core/DBContext.java
@@ -2,6 +2,8 @@ package org.hoann.prj301.core;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -13,14 +15,14 @@ public class DBContext {
     private static final String DB_PASSWORD = "12345";
     private static DBContext instance;
 
-    //Single-ton Design Pattern
+    // Single-ton Design Pattern
     public static DBContext getInstance() {
         if (instance == null) {
             instance = new DBContext();
         }
         return instance;
     }
-    
+
     private static void log(String message, Exception ex) {
         Logger.getLogger(DBContext.class.getName())
                 .log(Level.SEVERE, message, ex);
@@ -33,9 +35,39 @@ public class DBContext {
             String url = "jdbc:sqlserver://localhost:1433;DatabaseName=" + DB_NAME
                     + ";encrypt=true;trustServerCertificate=true;";
             conn = DriverManager.getConnection(url, DB_USERNAME, DB_PASSWORD);
-        }catch(SQLException | ClassNotFoundException e){
+        } catch (SQLException | ClassNotFoundException e) {
             log("Exception on getConnection: ", e);
         }
         return conn;
+    }
+
+    public void closeConnection(Connection conn) {
+        try {
+            if (conn != null && !conn.isClosed()) {
+                conn.close();
+            }
+        } catch (SQLException e) {
+            log("Exception on closeConnection", e);
+        }
+    }
+
+    public void closePreparedStatement(PreparedStatement stm) {
+        try {
+            if (stm != null && !stm.isClosed()) {
+                stm.close();
+            }
+        } catch (SQLException e) {
+            log("Exception on closeConnection", e);
+        }
+    }
+
+    public void closeResultSet(ResultSet rs) {
+        try {
+            if (rs != null && !rs.isClosed()) {
+                rs.close();
+            }
+        } catch (SQLException e) {
+            log("Exception on closeConnection", e);
+        }
     }
 }
