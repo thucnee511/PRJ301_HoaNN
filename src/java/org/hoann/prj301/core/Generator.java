5984fd6fdf024c6dccf8d17a019196bedf66bbec
ngocngocthuc@gmail.com <ngocngocthuc@gmail.com>
2024-03-19 19:10:51 +0100

    build(core): Generator class

--- src/java/org/hoann/prj301/core/Generator.java
+++ src/java/org/hoann/prj301/core/Generator.java
@@ -1,5 +1,17 @@
 package org.hoann.prj301.core;
 
-public class Generator {
+import java.util.UUID;
 
+public final class Generator {
+
+    private static final int UUID_LENGTH = 10;
+
+    public static String generateUUID() {
+        UUID uuid = UUID.randomUUID();
+        String current = Long.toString(System.currentTimeMillis());
+        return (current + uuid.toString())
+                .replaceAll("-", "")
+                .replaceAll("\\D", "")
+                .substring(0, UUID_LENGTH);
+    }
 }
