6d903fbc7ec4f41fab48ffa3031924f56a530949
ngocngocthuc@gmail.com <ngocngocthuc@gmail.com>
2024-03-19 10:49:24 +0100

    feat: add ApplicationConfig.java file to config the main endpoint

---
+++ src/java/org/hoann/prj301/config/ApplicationConfig.java
@@ -0,0 +1,33 @@
+package org.hoann.prj301.config;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ApplicationConfig extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
